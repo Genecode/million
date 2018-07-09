3f49afb6cb19d59b39a8e6940be464cc97650014
Genecode <genecode@yandex.ru>
2018-07-10 00:35:16 +0300

    add models test

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+require 'support/my_spec_helper'
+
+RSpec.describe Game, type: :model do
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:game_w_questions) { FactoryBot.create(:game_with_factory, user: user) }
+
+  context 'Game Factory' do
+    it 'Game.create_game_for_user! new correct game' do
+      generate_questions(60)
+      game = nil
+
+      # Создaли игру, обернули в блок, на который накладываем проверки
+      expect {
+        game = Game.create_game_for_user!(user)
+        # Проверка: Game.count изменился на 1 (создали в базе 1 игру)
+      }.to change(Game, :count).by(1).and(
+        # GameQuestion.count +15
+        change(GameQuestion, :count).by(15).and(
+          # Game.count не должен измениться
+          change(Question, :count).by(0)
+        )
+      )
+
+      # Проверяем статус и поля
+      expect(game.user).to eq(user)
+      expect(game.status).to eq(:in_progress)
+
+      # Проверяем корректность массива игровых вопросов
+      expect(game.game_questions.size).to eq(15)
+      expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+    end
+  end
+
+  context 'game mechanic' do
+    it 'answer correct continues' do
+      level = game_w_questions.current_level
+      q = game_w_questions.current_game_question
+      expect(game_w_questions.status).to eq(:in_progress)
+
+      game_w_questions.answer_current_question!(q.correct_answer_key)
+
+      expect(game_w_questions.current_level).to eq(level + 1)
+
+#      expect(game_w_questions.previous_game_question).to eq q
+      expect(game_w_questions.current_game_question).not_to eq q
+
+      expect(game_w_questions.status).to eq(:in_progress)
+      expect(game_w_questions.finished?).to be_falsey
+    end
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  context 'validation check' do
+    it { should validate_presence_of :text }
+    it { should validate_presence_of :level }
+
+    it { should validate_inclusion_of(:level).in_range(0..14) }
+  end
+end
